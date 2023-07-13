703b3c6b0f70d23561cf5b5c1eec74c067dba90e
BLAERx <132937420+BLAERx@users.noreply.github.com>
2023-07-13 21:08:44 +0300

    start

---
+++ main.py
@@ -0,0 +1,11 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
+
+asyncio def main() -> None:
+    bot = Bot(token=)
+    dp = Dispatcher(bot)
+
+
+if __name__ = '__main__':
+    asyncio.run(main())
