c5e9df92b090cf525d406360965b9d7f9f0a2e3a
BLAERx <132937420+BLAERx@users.noreply.github.com>
2023-07-13 21:46:55 +0300

    smth don't work

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import asyncio
+import os
+
+from dotenv import load_dotenv
+from bot.handlers.user_handlers import register_user_handlers
 
 from aiogram import Bot, Dispatcher, types
+from bot.tk import TOKEN_API
 
-asyncio def main() -> None:
-    bot = Bot(token=)
+def register_handler(dp: Dispatcher) -> None:
+    register_user_handlers(dp)
+
+
+async def main() -> None:
+    """Entry point"""
+    token = TOKEN_API
+    bot = Bot(token)
     dp = Dispatcher(bot)
 
+    register_handler(dp)
+    try:
+        await dp.start_polling()
+    except Exception as _ex:
+        print(f'There is exception -- {_ex}')
 
-if __name__ = '__main__':
+if __name__ == "__main__":
     asyncio.run(main())
